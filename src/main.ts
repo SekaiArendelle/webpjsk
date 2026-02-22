02b502f7a6782c55340427124bbe279089f064a2
Arendelle <Arendelle_@outlook.com>
2026-02-22 09:14:13 +0800

    reformat the whole project

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import './style.css'
-import { render } from './render';
+import "./style.css";
+import { render } from "./render";
 
-const app = document.getElementById('app') as HTMLDivElement;
+const app = document.getElementById("app") as HTMLDivElement;
 app.innerHTML = `
   <div>
     <canvas id="gameCanvas"></canvas>
   </div>
 `;
 
-render(document.getElementById('gameCanvas') as HTMLCanvasElement);
+render(document.getElementById("gameCanvas") as HTMLCanvasElement);
