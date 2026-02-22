9dd588bde97b42772e0e7433093d7cc4c690aced
Arendelle <Arendelle_@outlook.com>
2026-02-22 09:07:34 +0800

    Staged complete rendering charts (#1)
    * init
    
    * UI: support drag,tap,flick note
    
    ---------
    
    Co-authored-by: gushishang <117088703+gushishang@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import './style.css'
+import { render } from './render';
 
 const app = document.getElementById('app') as HTMLDivElement;
 app.innerHTML = `
   <div>
-    <h1>Hello world!</h1>
+    <canvas id="gameCanvas"></canvas>
   </div>
 `;
+
+render(document.getElementById('gameCanvas') as HTMLCanvasElement);
